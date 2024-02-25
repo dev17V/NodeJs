b9426f00a3e7b30d5313dd346500ca6041c77d07
ACKER POSIDEN <94275268+dev17V@users.noreply.github.com>
2024-02-25 13:25:45 -0600

    Update caculator.js

--- caculator.js
+++ caculator.js
@@ -1,93 +1,69 @@
-//goal: make a caculator in javascript <_<
-
-///////////////////////////////
-
-const prompt = require("prompt-sync");
-const readline = require("readline");
-
-///////////////////////////////
-
-
-var sum; // addition answer
-
-var difference; // subtraction answer
-
-var quotient; // division answer
-
-var product; // multiplication answer
-
-main();
+const prompt = require("prompt-sync")();
 
 function addStuff() {
-
-	var z;
-
-	var num1 = 5; // wanna turn this into a user input
-    var num2 = 5; // wanna turn this into a user input
-
+    var num1 = parseFloat(prompt("Enter the first number: "));
+    var num2 = parseFloat(prompt("Enter the second number: "));
     var z = num1 + num2;
-
     console.log("Your Sum Is: " + z);
 }
 
 function subtractStuff() {
-	var z;
-
-	var num1 = 5; // wanna turn this into a user input
-    var num2 = 5; // wanna turn this into a user input
-
+    var num1 = parseFloat(prompt("Enter the first number: "));
+    var num2 = parseFloat(prompt("Enter the second number: "));
     var z = num1 - num2;
-
     console.log("Your Difference Is: " + z);
 }
 
 function devideStuff() {
-
-	var z;
-
-	var num1 = 5; // wanna turn this into a user input
-    var num2 = 5; // wanna turn this into a user input
-
-    var z = num1 / num2;
-
-    console.log("Your Quotient Is: " + z);
-
+    var num1 = parseFloat(prompt("Enter the first number: "));
+    var num2 = parseFloat(prompt("Enter the second number: "));
+    if (num2 === 0) {
+        console.log("Cannot divide by zero.");
+    } else {
+        var z = num1 / num2;
+        console.log("Your Quotient Is: " + z);
+    }
 }
 
-function mutiplyStuff() {
-
-	var z;
-
-	var num1 = 5; // wanna turn this into a user input
-    var num2 = 5; // wanna turn this into a user input
-
+function multiplyStuff() {
+    var num1 = parseFloat(prompt("Enter the first number: "));
+    var num2 = parseFloat(prompt("Enter the second number: "));
     var z = num1 * num2;
-
     console.log("Your Product Is: " + z);
+}
 
+function findDivisor() {
+    var num = parseFloat(prompt("Enter the number to find divisors: "));
+    console.log("Divisors of " + num + " are: ");
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
+            console.log(i);
+        }
+    }
 }
 
 function main() {
+    console.clear();
 
-	console.clear();
+    console.log("                      Calculator Made In Js By Furher Acker                       \n");
 
-	console.log("                      Caculator Made In Js By Furher Acker                       \n");
+    console.log("           ║ 1 | add\n           ║ 2 | subtract\n           ║ 3 | divide\n           ║ 4 | multiply\n           ║ 5 | find divisor\n");
 
-	console.log("           ║ 1 | add\n           ║ 2 | subtract\n           ║ 3 | devide\n           ║ 4 | multiply\n");
+    const calculate_option = prompt("Enter your choice (1-5): ");
 
-	const caculate_option = "4"; //just put a number between 1-4 in the "" to choose your problem type
-
-	if (caculate_option == "1") {
-		addStuff();
-	} else if (caculate_option == "2") {
-	       subtractStuff();
-	} else if (caculate_option == "3") {
-		devideStuff();
-	} else if (caculate_option == "4") {
-		mutiplyStuff();
-	} else if (caculate_option == null || caculate_option == "" || caculate_option == "a", "b", "c", "d") {
-		console.log("\n[x] { " +caculate_option + " } is not a valid number");
-	} else {
-		console.log("GoodBye <_<");
-	}
+    if (calculate_option === "1") {
+        addStuff();
+    } else if (calculate_option === "2") {
+        subtractStuff();
+    } else if (calculate_option === "3") {
+        devideStuff();
+    } else if (calculate_option === "4") {
+        multiplyStuff();
+    } else if (calculate_option === "5") {
+        findDivisor();
+    } else {
+        console.log("\n[x] { " + calculate_option + " } is not a valid number");
+    }
 }
+
+main();
